ee6dff3d024df0993239a7f8fa90d37f19403248
Сергей Милантьев <geerkus@gmail.com>
2020-05-16 22:01:16 +0300

    Document Context Matrix, pLSA

--- vectorization/metrics.py
+++ vectorization/metrics.py
@@ -4,20 +4,20 @@ import numpy as np
 import pandas as pd
 
 
-def _get_cosine_similarity(a, b):
+def get_cosine_similarity(a, b):
     numerator, denominator = sum([a[i] * b[i] for i in range(len(a))]), \
                              math.sqrt(sum([a[i] * a[i] for i in range(len(a))]) *
                                        sum([b[i] * b[i] for i in range(len(a))]))
     return numerator / denominator
 
 
-def _get_weighted_jaccard_similarity(s, t):
+def get_weighted_jaccard_similarity(s, t):
     numerator, denominator = sum([min(s[i], t[i]) for i in range(len(s))]), \
                                sum([max(s[i], t[i]) for i in range(len(s))])
     return numerator / denominator
 
 
-def _get_kullback_leibler_divergence(p, q, positive):
+def get_kullback_leibler_divergence(p, q, positive):
     with np.errstate(divide='ignore'):
         df = p.mul(np.log(p / q), fill_value=0)
         df[np.isinf(df)] = 0.0
@@ -30,26 +30,26 @@ def _get_kullback_leibler_divergence(p, q, positive):
         return -result
 
 
-def _get_jensen_shannon_divergence(p, q, positive):
+def get_jensen_shannon_divergence(p, q, positive):
     m = pd.Series((p + q) / 2)
-    return _get_kullback_leibler_divergence(p, m, positive) / 2 + _get_kullback_leibler_divergence(q, m, positive) / 2
+    return get_kullback_leibler_divergence(p, m, positive) / 2 + get_kullback_leibler_divergence(q, m, positive) / 2
 
 
-def get_cosine_similarity(coord, words, ppmi_df):
+def get_cosine_similarity_linkage(coord, words, ppmi_df):
     i, j = coord
-    return _get_cosine_similarity(ppmi_df[words[i]], ppmi_df[words[j]])
+    return get_cosine_similarity(ppmi_df[words[i]], ppmi_df[words[j]])
 
 
-def get_weighted_jaccard_similarity(coord, words, ppmi_df):
+def get_weighted_jaccard_similarity_linkage(coord, words, ppmi_df):
     i, j = coord
-    return _get_weighted_jaccard_similarity(ppmi_df[words[i]], ppmi_df[words[j]])
+    return get_weighted_jaccard_similarity(ppmi_df[words[i]], ppmi_df[words[j]])
 
 
-def get_kullback_leibler_divergence(coord, words, ppmi_df, positive=True):
+def get_kullback_leibler_divergence_linkage(coord, words, ppmi_df, positive=True):
     i, j = coord
-    return _get_kullback_leibler_divergence(ppmi_df[words[i]], ppmi_df[words[j]], positive)
+    return get_kullback_leibler_divergence(ppmi_df[words[i]], ppmi_df[words[j]], positive)
 
 
-def get_jensen_shannon_divergence(coord, words, ppmi_df, positive=True):
+def get_jensen_shannon_divergence_linkage(coord, words, ppmi_df, positive=True):
     i, j = coord
-    return _get_jensen_shannon_divergence(ppmi_df[words[i]], ppmi_df[words[j]], positive)
+    return get_jensen_shannon_divergence(ppmi_df[words[i]], ppmi_df[words[j]], positive)
--- vectorization/vectorizer.py
+++ vectorization/vectorizer.py
@@ -22,6 +22,13 @@ def _get_word_set(_corpus):
     return _word_set
 
 
+def _get_document_set(_corpus):
+    _document_set = []
+    for i in range(len(_corpus)):
+        _document_set.append('document{}'.format(i))
+    return _document_set
+
+
 def _get_word_context_matrix(_corpus, _word_set, _window=5):
     word_count = len(_word_set)
     word_context_matrix = pd.DataFrame(np.zeros([word_count, word_count]), 
@@ -35,6 +42,19 @@ def _get_word_context_matrix(_corpus, _word_set, _window=5):
     return word_context_matrix
 
 
+def _get_document_context_matrix(_corpus, _word_set, _document_set, _window=5):
+    word_count = len(_word_set)
+    document_count = len(_corpus)
+    document_context_matrix = pd.DataFrame(np.zeros([document_count, word_count]),
+                                           columns=_word_set,
+                                           index=_document_set)
+    for i, _document in enumerate(_corpus):
+        for _sentence in _document:
+            for _word in _sentence:
+                document_context_matrix.iloc[i][_word] += 1
+    return document_context_matrix
+
+
 def _get_pmi(df, ppmi=True):
     col_totals = df.sum(axis=0)
     row_totals = df.sum(axis=1)
@@ -46,15 +66,15 @@ def _get_pmi(df, ppmi=True):
     df[np.isinf(df)] = 0.0
     if ppmi:
         df[df < 0] = 0.0
-    return df
+    return normalize_dataframe(df)
 
 
 def get_linkages(_indeces, _words, _ppmi_df):
-    cosine_weights = np.apply_along_axis(wm.get_cosine_similarity, 0, _indeces, _words, _ppmi_df)
-    jaccard_weights = np.apply_along_axis(wm.get_weighted_jaccard_similarity, 0, _indeces, _words, _ppmi_df)
-    kullback_leibler_weights = np.apply_along_axis(wm.get_kullback_leibler_divergence, 0, _indeces, _words, _ppmi_df,
+    cosine_weights = np.apply_along_axis(wm.get_cosine_similarity_linkage, 0, _indeces, _words, _ppmi_df)
+    jaccard_weights = np.apply_along_axis(wm.get_weighted_jaccard_similarity_linkage, 0, _indeces, _words, _ppmi_df)
+    kullback_leibler_weights = np.apply_along_axis(wm.get_kullback_leibler_divergence_linkage, 0, _indeces, _words, _ppmi_df,
                                                    positive=False)
-    jensen_shannon_weights = np.apply_along_axis(wm.get_jensen_shannon_divergence, 0, _indeces, _words, _ppmi_df,
+    jensen_shannon_weights = np.apply_along_axis(wm.get_jensen_shannon_divergence_linkage, 0, _indeces, _words, _ppmi_df,
                                                  positive=True)
     return [linkage(cosine_weights, 'ward'),
             linkage(jaccard_weights, 'ward'),
@@ -91,19 +111,27 @@ def draw(_wordsim_pairs, _ppmi_df, _word_context_df, _word_set):
     _draw_matrix_heatmap(_word_context_df, _word_set)
 
 
-def compare_word(_ppmi_df, w1, w2):
+def compare_word(_df, _w1, _w2, base=False, word2vec=False):
+    if base:
+        _w1_df, _w2_df = _df[_w1], _df[_w2]
+    elif word2vec:
+        pass
+    else:
+        _w1_df, _w2_df = _df.loc[:, _w1], _df.loc[:, _w2]
+
     wordsim353 = pandas.read_csv('docs/wordsim353.csv')
-    print('{} - {}'.format(w1, w2))
-    wordsim = wordsim353[(wordsim353['Word 1'] == w1) & (wordsim353['Word 2'] == w2) |
-                         (wordsim353['Word 1'] == w2) & (wordsim353['Word 2'] == w1)]
+    wordsim353['Human (Mean)'] = normalize_series(wordsim353['Human (Mean)'])
+    print('{} - {}'.format(_w1, _w2))
+    wordsim = wordsim353[(wordsim353['Word 1'] == _w1) & (wordsim353['Word 2'] == _w2) |
+                         (wordsim353['Word 1'] == _w2) & (wordsim353['Word 2'] == _w1)]
     print(wordsim)
-    cosine = wm._get_cosine_similarity(_ppmi_df[w1], _ppmi_df[w2])
+    cosine = wm.get_cosine_similarity(_w1_df, _w2_df)
     print('Cosine: {}'.format(cosine))
-    jaccard = wm._get_weighted_jaccard_similarity(_ppmi_df[w1], _ppmi_df[w2])
+    jaccard = wm.get_weighted_jaccard_similarity(_w1_df, _w2_df)
     print('Jaccard: {}'.format(jaccard))
-    kullback_leibler = wm._get_kullback_leibler_divergence(_ppmi_df[w1], _ppmi_df[w2], positive=False)
+    kullback_leibler = wm.get_kullback_leibler_divergence(_w1_df, _w2_df, positive=False)
     print('Kullback-Leibler: {}'.format(kullback_leibler))
-    jensen_shannon = wm._get_jensen_shannon_divergence(_ppmi_df[w1], _ppmi_df[w2], positive=True)
+    jensen_shannon = wm.get_jensen_shannon_divergence(_w1_df, _w2_df, positive=True)
     print('Jensen-Shannon: {}'.format(jensen_shannon))
     result = {'wordsim353': wordsim.iloc[0]['Human (Mean)'], 'cosine': cosine, 'jaccard': jaccard,
               'kullback_leibler': kullback_leibler, 'jensen_shannon': jensen_shannon}
@@ -121,37 +149,99 @@ def get_pearson_coef(_x, _y):
     return numerator / (denominator_x * denominator_y)**0.5
 
 
-def main():
-    dict_files = {('bank', 'money'): 9, ('book', 'paper'): 8, ('coffee', 'cup'): 8, ('computer', 'internet'): 8,
-                  ('doctor', 'nurse'): 8, ('dollar', 'loss'): 8, ('football', 'soccer'): 8, ('phone', 'cell'): 8,
-                  ('software', 'computer'): 8, ('tiger', 'cat'): 11}
-    result = []
-    for group_names, count in dict_files.items():
-        first_word, second_word = group_names
-        corpus = hp.get_corpus(first_word, count)
-        corpus = [[lm.lemmatize_sentence(sentence) for sentence in tk.custom_sentence_tokenize(doc)] for doc in corpus]
-        word_set = _get_word_set(corpus)
-        word_context_df = _get_word_context_matrix(corpus, word_set)
-        ppmi_df = _get_pmi(word_context_df)
-        result.append(compare_word(ppmi_df, first_word, second_word))
-        # wordsim_pairs = [(first_word, second_word)]
-        # draw(wordsim_pairs, ppmi_df, word_context_df, word_set)
+def normalize_dataframe(_df):
+    _result = _df.copy()
+    for _feature_name in _df.columns:
+        _max_value = _df[_feature_name].max()
+        _min_value = _df[_feature_name].min()
+        _result[_feature_name] = (_df[_feature_name] - _min_value) / (_max_value - _min_value)
+    return _result
 
+
+def normalize_series(_series, min_max=True):
+    if min_max:
+        return (_series - _series.min()) / (_series.max() - _series.min())
+    else:
+        return (_series - _series.mean()) / _series.std()
+
+
+def plsa(_corpus, _word_set, _document_set, number_of_topics=5, max_iterations=1):
+    word_count = len(_word_set)
+    document_count = len(_document_set)
+
+    document_context_matrix = _get_document_context_matrix(_corpus, _word_set, _document_set)
+    document_topic_probability = normalize_dataframe(
+        pd.DataFrame(np.random.random(size=(document_count, number_of_topics)), index=_document_set))
+    topic_word_probability = normalize_dataframe(pd.DataFrame(np.random.random(size=(number_of_topics, word_count)),
+                                                              columns=_word_set))
+    topic_probability = np.zeros([document_count, word_count, number_of_topics])
+
+    # E-Step
+    for iteration in range(max_iterations):
+        for _i, _doc in enumerate(_document_set):
+            for _j, _word in enumerate(_word_set):
+                probability = document_topic_probability.loc[_doc, :] * topic_word_probability.loc[:, _word]
+                topic_probability[_i][_j] = normalize_series(probability)
+    # M-Step
+    for _topic in range(number_of_topics):
+        for _j, _word in enumerate(_word_set):
+            result = 0
+            for _i, _doc in enumerate(_document_set):
+                result += document_context_matrix.loc[_doc][_word] * topic_probability[_i, _j, _topic]
+            topic_word_probability.loc[_topic][_word] = result
+        topic_word_probability.loc[_topic] = normalize_series(topic_word_probability.loc[_topic])
+    for _i, _doc in enumerate(_document_set):
+        for _topic in range(number_of_topics):
+            result = 0
+            for _j, _word in enumerate(_word_set):
+                result += document_context_matrix.loc[_doc][_word] * topic_probability[_i, _j, _topic]
+            document_topic_probability.loc[_doc][_topic] = result
+        document_topic_probability.loc[_doc] = normalize_series(document_topic_probability.loc[_doc])
+    return topic_word_probability
+
+
+def show(_result, _method):
     wordsim_values, cosine_values, jaccard_values, kullback_leibler_values, jensen_shannon_values = [], [], [], [], []
-
-    for dict_item in result:
+    for dict_item in _result:
         wordsim_values.append(dict_item['wordsim353'])
         cosine_values.append(dict_item['cosine'])
         jaccard_values.append(dict_item['jaccard'])
         kullback_leibler_values.append(dict_item['kullback_leibler'])
         jensen_shannon_values.append(dict_item['jensen_shannon'])
-    print('###################')
+    print(_method)
     print('Pearson coef (wordsim and cosine): {}'.format(get_pearson_coef(wordsim_values, cosine_values)))
     print('Pearson coef (wordsim and jaccard_values): {}'.format(get_pearson_coef(wordsim_values, jaccard_values)))
     print('Pearson coef (wordsim and kullback_leibler_values): {}'.format(get_pearson_coef(wordsim_values,
                                                                                            kullback_leibler_values)))
     print('Pearson coef (wordsim and jensen_shannon_values): {}'.format(get_pearson_coef(wordsim_values,
                                                                                          jensen_shannon_values)))
+
+
+def main():
+    dict_files = {('bank', 'money'): 9, ('book', 'paper'): 8, ('coffee', 'cup'): 8, ('computer', 'internet'): 8,
+                  ('doctor', 'nurse'): 8, ('dollar', 'loss'): 8, ('football', 'soccer'): 8, ('phone', 'cell'): 8,
+                  ('software', 'computer'): 8, ('tiger', 'cat'): 11}
+    result_base, result_plsa, result_word2vec = [], [], []
+    for group_names, count in dict_files.items():
+        first_word, second_word = group_names
+        corpus = hp.get_corpus(first_word, count)
+        corpus = [[lm.lemmatize_sentence(sentence) for sentence in tk.custom_sentence_tokenize(doc)] for doc in corpus]
+
+        word_set = _get_word_set(corpus)
+        document_set = _get_document_set(corpus)
+
+        result_plsa.append(compare_word(plsa(corpus, word_set, document_set), first_word, second_word))
+
+        word_context_df = _get_word_context_matrix(corpus, word_set)
+        ppmi_df = _get_pmi(word_context_df)
+        result_base.append(compare_word(ppmi_df, first_word, second_word, base=True))
+
+        # wordsim_pairs = [(first_word, second_word)]
+        # draw(wordsim_pairs, ppmi_df, word_context_df, word_set)
+
+    show(result_base, 'BASE METHOD')
+    show(result_plsa, 'PLSA METHOD')
+    # show(result_word2vec, 'WORD2VEC METHOD')
 
 
 main()
